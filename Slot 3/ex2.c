ce03ab29914df18ac64bd471aacd3389d7b12d54
KhongPhaiNhen <daoxuanthinh.6886@gmail.com>
2026-01-14 08:46:03 +0700

    update ex2

--- Slot 3/ex2.c
+++ Slot 3/ex2.c
@@ -4,21 +4,21 @@ int main(){
 	char kt = 'A';
 	
 	printf("%-15s %-15s %-15s %-15s %-15s %-15s\n", "Name", "Value", "Dec", "Hex", "Oct", "Size");
-	printf("%-15s %-15c %-15u %-15x %-15o %-15s\n\n", "kt", kt, kt, kt, kt, "1" );
+	printf("%-15s %-15c %-15u %-15x %-15o %-15s\n\n", "kt", kt, kt, kt, kt, sizeof(kt));
 	
-	int integerVar = 10;
-	long longVar = 1000;
-	float floatVar = 13.5f;
-	double doubleVar = 100.15;
-	
-	printf("%-15s\n", "Size (Bytes)");
-	printf("----------------------------------\n");
-	
-	printf("charVar: %-15d\n",sizeof(kt));
-	printf("integerVar: %-15d\n",sizeof(integerVar));
-	printf("longVar: %-15d\n",sizeof(longVar));
-	printf("floatVar: %-15d\n",sizeof(floatVar));
-	printf("doubleVar: %-15d\n",sizeof(doubleVar));
-
+//	int integerVar = 10;
+//	long longVar = 1000;
+//	float floatVar = 13.5f;
+//	double doubleVar = 100.15;
+//	
+//	printf("%-15s\n", "Size (Bytes)");
+//	printf("----------------------------------\n");
+//	
+//	printf("charVar: %-15d\n",sizeof(kt));
+//	printf("integerVar: %-15d\n",sizeof(integerVar));
+//	printf("longVar: %-15d\n",sizeof(longVar));
+//	printf("floatVar: %-15d\n",sizeof(floatVar));
+//	printf("doubleVar: %-15d\n",sizeof(doubleVar));
+//
 	
 }
