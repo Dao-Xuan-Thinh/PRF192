7776938c03db3d11903f092130764d4735f1b55b
KhongPhaiNhen <daoxuanthinh.6886@gmail.com>
2026-01-21 08:36:22 +0700

    push slot 5

---
+++ Slot 5/ex3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+
+//a4 b5 c3
+
+int main(){
+	float a, b, c, max;
+	
+	printf("Nhap a, b, c: ");
+	scanf("%f %f %f", &a, &b, &c);
+	
+	if(a>b && a>c)
+		max = a;
+		
+	else if(b>a && b>c)
+		max = b;
+		
+	else
+		max = c;
+		
+	printf("%.2f", max);
+}
